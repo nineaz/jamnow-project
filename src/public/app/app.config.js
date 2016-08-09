1c7c9416c2436b7bb75c20b106fdcf466ec9a0d6
CecileCee <cecile.muloway@gmail.com>
2016-08-09 14:55:18 +0200

    3 pages inscription (dont formulaire et confirmation)

--- src/public/app/app.config.js
+++ src/public/app/app.config.js
@@ -1,5 +1,5 @@
 jamApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
-	
+
 	// Default route
 	$urlRouterProvider.otherwise('/');
 
@@ -9,6 +9,18 @@ jamApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $
 	.state('home',  {
 		url: "/",
 		templateUrl: "../views/pages/home.html"
+	})
+	.state('inscription',  {
+		url: "/",
+		templateUrl: "../views/pages/inscription.html"
+	})
+	.state('inscription2',  {
+		url: "/",
+		templateUrl: "../views/pages/inscription2_form.html"
+	})
+	.state('inscription3',  {
+		url: "/",
+		templateUrl: "../views/pages/inscription3_confirmation.html"
 	});
 
 }]);
