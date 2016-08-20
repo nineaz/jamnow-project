d623234dc0d50ff4545e444b3e472097082221da
Nossyra46 <nossyra@gmail.com>
2016-08-20 15:15:27 +0200

    authentication on the run

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
+var bcrypt = require('bcryptjs');
 // Models
 
 var User = require('../models/user');
@@ -11,6 +12,9 @@ var Contact = require('../models/contact');
 // Routes
 // user routes
 User.methods(['get', 'put', 'post', 'delete']);
+
+User.before('post', hash_password).before('put', hash_password);
+
 User.register(router, '/users');
 
 // jam routes
@@ -21,10 +25,29 @@ Jam.register(router, '/jams');
 Contact.methods(['get', 'post']);
 Contact.register(router, '/contacts');
 
+// login routes
+
+router.post('/login', function(req, res) {
+	
+});
+
 // hashing passwords
+function hash_password(req, res, next) {
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(req.body.password, salt, function(err, hash) {
+	        req.body.password = hash;
+	        next();
+	    });
+	});
+}
 
 
 
+// comparing passwords
+
+// function compare_passwords() {
+
+// }
 
 // Return router
 
