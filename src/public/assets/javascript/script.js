62d0d6beb0bff485879412524929fd1587c20bc0
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-08-27 14:06:05 +0200

    page connexion ok sauf le lien en bas

---
+++ src/public/assets/javascript/script.js
@@ -0,0 +1,6 @@
+// MENU RESPONSIVE MATERIALIZE
+alert("le js fonctionne");
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
