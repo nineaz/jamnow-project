3e59d78d574a9f993764d8c5d03db9114e994679
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-08-28 12:51:23 +0200

    page connexion mobil + desktop finished

--- src/public/assets/javascript/script.js
+++ src/public/assets/javascript/script.js
@@ -1,5 +1,5 @@
 // MENU RESPONSIVE MATERIALIZE
-alert("le js fonctionne");
+console.log("le js fonctionne");
   // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
